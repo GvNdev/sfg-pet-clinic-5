51c27e30334ba4498e2bf93e460967b6973de76e
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-17 12:09:37 -0500

    Initial Project Add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sfg-pet-clinic-5
+
 SFT Pet Clinic 
---
+++ src/main/java/com/stmc/sfgpetclinic5/SfgPetClinic5Application.java
@@ -0,0 +1,13 @@
+package com.stmc.sfgpetclinic5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinic5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinic5Application.class, args);
+    }
+
+}
