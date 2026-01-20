89c7b1f9b7d81662980d2da7a3b751d502cf0e66
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-19 23:52:21 -0500

    Refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/com/stmc/sfgpetclinic5/services/VetService.java
+++ pet-clinic-data/src/main/java/com/stmc/sfgpetclinic5/services/VetService.java
@@ -2,10 +2,5 @@ package com.stmc.sfgpetclinic5.services;
 
 import com.stmc.sfgpetclinic5.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
