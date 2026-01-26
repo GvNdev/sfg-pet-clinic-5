16cf79f0aaf4cef754db236f229482cd34930b08
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-25 20:38:14 -0500

    Added Spring Data JPA service for Owners. Closes #40

--- pet-clinic-data/src/main/java/com/stmc/sfgpetclinic5/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/stmc/sfgpetclinic5/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.stmc.sfgpetclinic5.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
