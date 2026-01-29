e317fa393eb411a392a5c5f568e47fad5ffe83f0
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-29 13:22:02 -0500

    Impl find owner. Closes #54

--- pet-clinic-data/src/main/java/com/stmc/sfgpetclinic5/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/stmc/sfgpetclinic5/services/OwnerService.java
@@ -2,6 +2,9 @@ package com.stmc.sfgpetclinic5.services;
 
 import com.stmc.sfgpetclinic5.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
