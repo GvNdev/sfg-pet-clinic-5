dda1bf67cc5a6ad4bc5789806061a01b3e41b545
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-01-31 23:11:29 -0500

    Added Vets JSON Endpoint. Closes #69

--- pet-clinic-web/src/main/java/com/stmc/sfgpetclinic5/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/stmc/sfgpetclinic5/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.stmc.sfgpetclinic5.controllers;
 
+import com.stmc.sfgpetclinic5.model.Vet;
 import com.stmc.sfgpetclinic5.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +22,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 }
